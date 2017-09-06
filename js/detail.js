7a69c8249e15ea88c7a6efe4e107a083528e0f46
yjchen <728480581@qq.com>
2017-09-06 14:55:23 +0800

    updata

--- js/detail.js
+++ js/detail.js
@@ -1,11 +1,20 @@
 ﻿
 $(function(){
-	$.get("../data/requirejs.json",function(d){
-		console.log(d);
+	$.extend({
+		getQueryString: function (name) {
+			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+			var r = window.location.search.substr(1).match(reg); 
+			if (r != null) return unescape(r[2]); return null; 
+		}
+	})
+	var data=$.getQueryString("data");
+	$.get("../data/"+data+".txt",function(d){
+		console.log(d)
+		var data=JSON.parse(d)
 		var vm=new Vue({
 			el:"#content",
 			data:{
-				"article":d
+				"article":data
 			}
 		});
 	})		
