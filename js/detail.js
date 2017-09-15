7bc2a8674228a5a217c057f4159e98b159cfba59
YujieCheng <728480581@qq.com>
2017-09-15 23:41:35 +0800

    a

--- js/detail.js
+++ js/detail.js
@@ -21,7 +21,23 @@ $(function(){
 				}
 			});
 			jQuery.syntax({theme: 'paper', blockLayout: 'fixed'});
+			$('a[href*=#],area[href*=#]').click(function() {  
+				console.log(this.pathname)  
+				if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {  
+					var $target = $(this.hash);  
+					$target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');  
+					if ($target.length) {  
+						var targetOffset = $target.offset().top-50;  
+						$('html,body').animate({  
+									scrollTop: targetOffset  
+								},  
+								1000);  
+						return false;  
+					}  
+				}  
+			}); 
 		});
 		
 	})
+	
 })
